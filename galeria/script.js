9deec6d5be443aa091e4a946c762813d8941e4d1
KMagdi <46417302+kmagdi@users.noreply.github.com>
2021-02-22 20:18:22 +0100

    thumbnails

--- galeria/script.js
+++ galeria/script.js
@@ -1,62 +1,71 @@
 let fotok =[
  {  
     photo: 'asset/1.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc1',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/2.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/3.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc3',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/4.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc4',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/5.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc5',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/6.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc6',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/7.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc7',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/8.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc8',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/9.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc9',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
   {  
     photo: 'asset/10.jpg',
-    title: 'Chamonix - Mont Blanc',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum earum eaque dolorum quis vel magnam laboriosam mollitia odio est explicabo adipisci veniam nam tenetur porro, optio animi consequatur illo dolorem.'
+    title: 'Chamonix - Mont Blanc10',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
   },
 ];
 
 showPhoto(0)
 fotok.forEach((obj,index)=>{
     $('.thumb').append(
-        `<img id='${index}' src='${obj.photo}' alt=''>`
+        `<div class='thumbnail'><img id='${index}' src='${obj.photo}'><div class='title'>${obj.title}</div></div>`
     )
-})
+
+   
+
+    $(`#${index}`).on('click',()=>{
+     showPhoto(index)
+     removeBorders()
+     $(`#${index}`).css('border','3px solid #6b22f3')
+    })
+  })
+
 
 $(()=>{
     $('.next').on('click',()=>{
@@ -70,6 +79,7 @@ $(()=>{
             showPhoto(getIndex(src)-1)
     })
 
+
 }) 
 
 const getIndex=(photoName)=>{
@@ -81,4 +91,9 @@ function showPhoto(i){
     $('#title').text(`${fotok[i].title}`)
     $('#descr').text(`${fotok[i].description}`)
     return
+}
+const removeBorders=()=>{
+  fotok.forEach((o,i)=>{
+    $(`#${i}`).css('border','1px solid #b088f9')
+  })
 }
